f7e60ad4baffbdac15c214815eb8c6e0c5427478
Marcel <42617645+OfficialPIGZ@users.noreply.github.com>
2021-11-08 11:13:44 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RunTrack 2 - jour 4 - job06</title>
+    <style type="text/css">
+table,
+td {
+    border: 1px solid #333;
+}
+
+thead,
+tfoot {
+    background-color: #333;
+    color: #fff;
+}
+</style>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th colspan="2">Testez si un nombre est pair, ou impair.</th>
+            </tr>
+        </thead>
+    <form method="GET" action="index.php">
+        <tbody>
+            <tr>
+        <td><label>Nombre :</label></td>
+        <td><input type="text" name="nombre"><br></td>
+        <td><input type="submit" value="Submit"></td>
+    </tr>
+        </tbody>
+        </form>
+    </table>
+    </body>
+</html>
+
+<?php
+$number = $_GET['nombre'];
+if ($number % 2 == 0)
+{
+    echo "$number est pair.";
+}
+else
+{
+    echo "$number est impair.";
+}
+?>
